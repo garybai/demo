0fe0bdc409d8c3feba6ebcbbb6851ef2a6c4b7ee
baiguangyu <490000133@qq.com>
2022-09-06 22:37:21 +0800

    test

--- springboot-test-demo/src/main/java/com/example/springboottestdemo/controller/HelloController.java
+++ springboot-test-demo/src/main/java/com/example/springboottestdemo/controller/HelloController.java
@@ -1,10 +1,9 @@
 package com.example.springboottestdemo.controller;
 
 import com.example.springboottestdemo.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * ceshi controller
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2020/5/6 15:41
  **/
 @RestController
+@Slf4j
 public class HelloController {
 
     @Autowired
@@ -26,6 +26,17 @@ public class HelloController {
     @RequestMapping("/test")
     public String test(@RequestParam("id") String id){
         return id;
+    }
+
+    @RequestMapping(value = "/addArticle", method = RequestMethod.POST)
+    @ResponseBody
+    public String addArticle(@RequestParam("article") String article) {
+//        String s = TextTool.generateArticleText(article);
+        String hash = SimHash.simHash(article).toString();
+        log.error("{}-------->{}", hash, article);
+//        boolean b = articleService.addArticle(articleid, article, type, SimHash.simHash(s).toString());
+//        return result.setData(b);
+        return hash;
     }
 
 }
